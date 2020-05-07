a835fc40374230dae81700e469e68425f9706006
Rowan McAlpin <37227254+Rowan-McAlpin@users.noreply.github.com>
2020-05-07 11:22:21 -0700

    Edit a ton of stuff, and learn std::cin.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
+  std::string name;
+  int age;
   std::cout << "Hello, World." << '\n';
   std::cout << "This is the first multi-line c++ prorgam I have made." << '\n';
+  std::cout << "What is your name? ";
+  std::cin >> name;
+  std::cout << "Hello, ";
+  std::cout << name << '\n';
+  std::cout << "How old are you? ";
+  std::cin >> age;
+  std::cout << "You are named " << name << ", and you are " << age << " years old." << '\n';
+  std::cout << "Thank you for playing! " << '\n';
   return 0;
 }
