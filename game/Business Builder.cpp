a0ec4946d6841ae0c1fa965979ada22b07e295ca
Rowan McAlpin <37227254+Rowan-McAlpin@users.noreply.github.com>
2020-05-10 08:48:38 -0700

    Edit a ton and try to learn more c++.

--- game/Business Builder.cpp
+++ game/Business Builder.cpp
@@ -3,8 +3,18 @@
 #include <chrono>         // See above.
 
 int main(int argc, char const *argv[]) {
+  int age;
   std::cout << "Hello, there! This is the game so far." << '\n';
   std::this_thread::sleep_for(std::chrono::seconds(2));
   std::cout << "This is hopefully two seconds later" << '\n';
+  std::cin >> age;
+  std::cout << "You are " << age << '\n';
+  if (age == 21) {
+    std::cout << "You are 21." << '\n';
+  } else if (age < 23) {
+    std::cout << "You are too young. Get out of the bar." << '\n';
+  } else {
+    std::cout << "You should try our new firewhisky." << '\n';
+  }
   return 0;
 }
--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -13,5 +13,10 @@ int main(int argc, char const *argv[]) {
   std::cin >> age;
   std::cout << "You are named " << name << ", and you are " << age << " years old." << '\n';
   std::cout << "Thank you for playing! " << '\n';
+  // define an integer variable named x
+  int x; // this variable is uninitialized because we haven't given it a value
+
+  // print the value of x to the screen
+  std::cout << x; // who knows what we'll get, because x is uninitialized
   return 0;
 }
